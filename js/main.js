713aba1dbd90c6cbba22bd31edbfa8e65187123f
yrDream <60001120+yrDream@users.noreply.github.com>
2021-03-24 13:29:11 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+    var p = document.querySelectorAll("#p_item");
+
+
+    $(p).fadeIn("slow", function() {
+        // Animation complete
+    });
+    console.log("123");
+
+
+
+});
